e82bc44608debdd72209c7e64f924ffc78f207af
Mihail Puica <mihail.puica@aero.bombardier.com>
2016-11-25 14:08:06 -0500

    proper markup

--- index.php
+++ index.php
@@ -1,2 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Foo</title>
+</head>
+<body>
 version : 1.1.0<br>
 date: <?php echo date("Y-m-d H:i:s") ?>
+
+</body>
+</html>
